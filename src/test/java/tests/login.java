f7d634c652e0d6635e9ec2cb816b9eebc6a96b77
Yavuz Semiz <yavuz.s25@gmail.com>
2023-02-14 15:33:13 +0100

    from github

--- src/test/java/tests/login.java
+++ src/test/java/tests/login.java
@@ -3,9 +3,5 @@ package tests;
 public class login {
     public static void main(String[] args) {
         System.out.println("run");
-        
-        
-        // from github to intellij
-        // from github to intellij
     }
 }
